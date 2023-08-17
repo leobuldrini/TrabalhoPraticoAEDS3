bbfc2577157b6b1a740877e09fe8318a0d073cc9
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2023-08-17 20:44:56 -0300

    3 examples of breaches formatted in toByteArray

--- src/models/Breach.java
+++ src/models/Breach.java
@@ -1,22 +1,81 @@
 package models;
 
+import java.io.*;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class Breach {
-    String company;
-    long records_lost;
-    Date date;
-    String detailedStory;
-    String[] sectorAndMethod;
 
-    Breach(String company, long records_lost, Date date, String detailedStory, String[] sectorAndMethod){
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withLocale(Locale.US);
+
+    public int id;
+    public String company;
+    public long recordsLost;
+    public LocalDate date;
+    public String detailedStory;
+    public String[] sectorAndMethod;
+
+    public Breach(int id, String company, long recordsLost, LocalDate date, String detailedStory, String[] sectorAndMethod){
+        this.id = id;
         this.company = company;
-        this.records_lost = records_lost;
+        this.recordsLost = recordsLost;
         this.date = date;
         this.detailedStory = detailedStory;
         this.sectorAndMethod = sectorAndMethod;
     };
+    public Breach(){};
 
-    Breach(){};
+    public String toString(){
+        DecimalFormat df= new DecimalFormat("#,##0.00");//formata o valor dos pontos
+        String fullList = "";
+        for(int i = 0; i < sectorAndMethod.length; i++){
+            fullList += sectorAndMethod[i] + ",";
+        }
+        fullList = fullList.substring(0, fullList.length() - 1);
+        return "\nID:"+id +
+                "\nCompany:"+this.company +
+                "\nRecords Lost:"+ df.format(recordsLost) +
+                "\nDate:"+ date +
+                "\nDetailed Story:"+ detailedStory +
+                "\nsectorAndMethod:"+ fullList;
+    }
 
+    public byte[] toByteArray() throws IOException {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+        String fullList = "";
+        for(int i = 0; i < sectorAndMethod.length; i++){
+            fullList += sectorAndMethod[i] + ",";
+        }
+        fullList = fullList.substring(0, fullList.length() - 1);
+
+        dos.writeInt(id);
+        dos.writeUTF(company);
+        dos.writeLong(recordsLost);
+        dos.writeUTF(date.toString());
+        dos.writeUTF(detailedStory);
+        dos.writeUTF(fullList);
+
+        return baos.toByteArray();
+    }
+
+    public void fromByteArray(byte ba[]) throws IOException{
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+        DataInputStream dis = new DataInputStream(bais);
+
+        id=dis.readInt();
+        company=dis.readUTF();
+        recordsLost=dis.readLong();
+        date=LocalDate.parse(dis.readUTF(), formatter);
+        detailedStory=dis.readUTF();
+
+        String fullList = dis.readUTF();
+        sectorAndMethod = fullList.split(",");
+    }
 }
